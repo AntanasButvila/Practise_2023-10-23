d2d03a116f245029176c5febd8bbe4bed30ef8f1
Butvila_Antanas <antanas.werwolf@gmail.com>
2023-10-23 21:35:43 +0400

    Added first program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Методы: воз-т значение и void
+// тип_метода ИмяМетода(параметр1, параметр2)
+// Параметры: два числа и знак арифм. операции
+
+int Calculate(int first, int second, char sign)
+{
+    if (sign == '+') // '' - только для char, буква "э" на англ. раскл
+    {
+        return first + second;
+    }
+    else if (sign == '-') // '' - только для char, буква "э" на англ. раскл
+    {
+        return first - second;
+    }
+    else if (sign == '*') // '' - только для char, буква "э" на англ. раскл
+    {
+        return first * second;
+    }
+    else // Другие "знаки" : все, кроме "+", "-", "*"
+    {
+        Console.WriteLine("Ошибка при вводе знака");
+        return 0;
+    }
+}
+
+// Используем метод - вызов
+// (int first, int second, char sign)
+Console.WriteLine(Calculate(2, 5, '+'));
+// first = 2, first = 5, char = '+'
+Console.WriteLine(Calculate(100, 35, '-'));
+// first = 100, first = 35, char = '-'
+Console.WriteLine(Calculate(100, 35, '*'));
+// first = 100, first = 35, char = '*'
+Console.WriteLine(Calculate(100, 35, '/'));
+// first = 100, first = 35, char = '/
